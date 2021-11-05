23c2b6ba4662faf5ca0a4db86654366b0c7202d3
NumenMirus <michaelbastauni@gmail.com>
2021-11-05 18:11:17 +0100

    finished generator

--- generator.py
+++ generator.py
@@ -8,6 +8,10 @@ from docx.shared import RGBColor
 from docx.shared import Mm
 from docx.oxml.shared import OxmlElement
 from docx.oxml.ns import qn
+from docx.table import _Cell
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
+from datetime import date
 
 def insertHR(paragraph, size, space):
     p = paragraph._p  # p is the <w:p> XML element
@@ -29,78 +33,260 @@ def insertHR(paragraph, size, space):
     bottom.set(qn('w:color'), 'auto')
     pBdr.append(bottom)
 
-d = Document()
+def set_cell_border(cell: _Cell, **kwargs):
+    """
+    Set cell`s border
+    Usage:
+    set_cell_border(
+        cell,
+        top={"sz": 12, "val": "single", "color": "#FF0000", "space": "0"},
+        bottom={"sz": 12, "color": "#00FF00", "val": "single"},
+        start={"sz": 24, "val": "dashed", "shadow": "true"},
+        end={"sz": 12, "val": "dashed"},
+    )
+    """
+    tc = cell._tc
+    tcPr = tc.get_or_add_tcPr()
 
-p1 = d.add_paragraph()
-p1.paragraph_format.line_spacing = 1
-p1.alignment = 0
-p1.add_run("Sara Ezelina Vantini\n").bold = True
-p1.add_run("via Don Carlo Gnocchi 19\n")
-p1.add_run("20148 MILANO\n")
-p1.add_run("Codice Fiscale:\n").bold = True
-p1.add_run("VNTSZL74E71F205T\n").bold = True
+    # check for tag existnace, if none found, then create one
+    tcBorders = tcPr.first_child_found_in("w:tcBorders")
+    if tcBorders is None:
+        tcBorders = OxmlElement('w:tcBorders')
+        tcPr.append(tcBorders)
+ 
+    # list over all available tags
+    for edge in ('start', 'top', 'end', 'bottom', 'insideH', 'insideV'):
+        edge_data = kwargs.get(edge)
+        if edge_data:
+            tag = 'w:{}'.format(edge)
+ 
+            # check for tag existnace, if none found, then create one
+            element = tcBorders.find(qn(tag))
+            if element is None:
+                element = OxmlElement(tag)
+                tcBorders.append(element)
+ 
+            # looks like order of attributes is important
+            for key in ["sz", "val", "color", "space", "shadow"]:
+                if key in edge_data:
+                    element.set(qn('w:{}'.format(key)), str(edge_data[key]))
 
-p2 = d.add_paragraph()
-p2.paragraph_format.line_spacing = 1
-p2.alignment = 2
-p2.add_run("Spett.le                                       \n")
-p2.add_run("la LUSac srl                              \n").bold = True
-p2.add_run("via Defendente Sacchi 13     \n")
-p2.add_run("27100 PAVIA                            \n\n")
-p2.add_run("Partita IVA 02778180188\n").bold = True
+def create_receipt(item_list, numero, causale):
+    data = str("Milano, "+ date.today().strftime("%d %B %Y"))
 
-p3 = d.add_paragraph()
-p3.paragraph_format.line_spacing = 1
-p3.alignment = 0
-p3.add_run("Ricevuta n 3/2021\n")
-p3.add_run("Milano 15 Giugno 2021\n")
-p3.add_run("Ricevo dalla Società la LUSac srl le somme sotto specificate a fronte delle prestazioni rientranti in rapporto di prestazione di lavoro autonomo occasionale*\n")
+    d = Document()
+
+    section = d.sections[0]
+    section.page_height = Mm(297)
+    section.page_width = Mm(210)
+    section.left_margin = Mm(25.4)
+    section.right_margin = Mm(25.4)
+    section.top_margin = Mm(25.4)
+    section.bottom_margin = Mm(25.4)
+    section.header_distance = Mm(12.7)
+    section.footer_distance = Mm(12.7)
+
+    style = d.styles['Normal']
+    font = style.font
+    font.name = 'Garamond'
+    font.size = Pt(10)
+
+    p1 = d.add_paragraph()
+    p1.paragraph_format.line_spacing = 1
+    p1.alignment = 0
+    p1.style = d.styles['Normal']
+    p1.add_run("Sara Ezelina Vantini\n").bold = True
+    p1.add_run("via Don Carlo Gnocchi 19\n")
+    p1.add_run("20148 MILANO\n")
+    p1.add_run("Codice Fiscale:\n").bold = True
+    p1.add_run("VNTSZL74E71F205T\n").bold = True
+
+    p2 = d.add_paragraph()
+    p2.paragraph_format.line_spacing = 1
+    p2.alignment = 2
+    p2.style = d.styles['Normal']
+    p2.add_run("Spett.le                                       \n")
+    p2.add_run("la LUSac srl                              \n").bold = True
+    p2.add_run("via Defendente Sacchi 13          \n")
+    p2.add_run("27100 PAVIA                             \n\n")
+    p2.add_run("Partita IVA 02778180188          \n").bold = True
+
+    p3 = d.add_paragraph()
+    p3.paragraph_format.line_spacing = 1
+    p3.alignment = 0
+    p3.style = d.styles['Normal']
+    p3.add_run("Ricevuta nr. "+str(numero)+" / "+date.today().strftime("%Y"))
+    p3.add_run("Milano 15 Giugno 2021\n")
+    p3.add_run("Ricevo dalla Società la LUSac srl le somme sotto specificate a fronte delle prestazioni rientranti in rapporto di prestazione di lavoro autonomo occasionale*\n")
 
 
-table = d.add_table(1,1, style="Light List")
-table.add_column(Mm(30))
-table.alignment = WD_TABLE_ALIGNMENT.CENTER
+    table = d.add_table(1,0, style="Light List")
+    table.add_column(Mm(129.2))
+    table.add_column(Mm(30))
+    table.autofit = True
+    table.alignment = WD_TABLE_ALIGNMENT.CENTER
 
-cell = table.rows[0].cells
-cell[0].text = "Tipologia della prestazione"
-cell[0].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
-cell[1].text = "costo netto cadauno"
-cell[1].horizontal_alignment = WD_TABLE_ALIGNMENT.CENTER
-cell[1].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
+    cell = table.rows[0].cells
+    cell[0].text = "Tipologia della prestazione"
+    cell[0].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
+    cell[1].text = "costo netto cadauno"
+    cell[1].horizontal_alignment = WD_TABLE_ALIGNMENT.CENTER
+    cell[1].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
 
-row0 = table.rows[0]
-row0.height = Mm(10)
+    row0 = table.rows[0]
+    row0.height = Mm(10)
 
-###################################################################################
+    d.add_paragraph()
 
-item_list = 
-
-for i in len(item_list):
-
-
-
+    table1 = d.add_table(1,0)
+    table1.add_column(Mm(129.2))
+    table1.add_column(Mm(30))
+    table1.alignment = WD_TABLE_ALIGNMENT.CENTER
 
 
 
-# table = d.add_table(1, 1)
-# table.alignment = WD_TABLE_ALIGNMENT.CENTER
-# table.add_column(Mm(30))
+    ###################################################################################
 
-# cell = table.rows[0].cells
-# cell[0].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
-# p0 = cell[0].add_paragraph()
-# p0.alignment = 1
-# run = p0.add_run("Tipologia della prestazione")
-# run.font.size = Pt(12)
-# run.font.name = 'Arial'
-# run.bold = True
-# insertHR(p0)
 
-# p1 = cell[1].add_paragraph()
-# p1.alignment = 1
-# run = p1.add_run("costo netto cadauno")
-# run.font.size = Pt(12)
-# run.font.name = 'Arial'
-# run.bold = True
+    for i in range(len(item_list)-1):
+        table1.add_row()
+    
+    row1 = table1.rows
 
-d.save("test.docx")
+    totale = 0.0
+    for i in range(len(item_list)):
+        totale = totale + (float(item_list[i][0]) * float(item_list[i][1]))
+        row1[i].cells[0].text = str(item_list[i][0])+"x "+item_list[i][2]
+        row1[i].cells[1].text = "€   " + str((item_list[i][1]))+"0"
+        i = i+1
+
+    table1.add_row()
+    row1 = table1.rows
+    
+    p4 = row1[-1].cells[1].add_paragraph()
+    p4.alignment = 0
+    p4.paragraph_format.line_spacing = 1
+    p4.add_run("\n\nTotale €  "+str(totale)+"0")
+
+    ######################################################################################
+
+    d.add_page_break()
+
+    p5 = d.add_paragraph()
+    p5.paragraph_format.line_spacing = 1
+    p5.alignment = 0
+    p5.style = d.styles['Normal']
+    p5.add_run("Sara Ezelina Vantini\n").bold = True
+    p5.add_run("via Don Carlo Gnocchi 19\n")
+    p5.add_run("20148 MILANO\n")
+    p5.add_run("Codice Fiscale:\n").bold = True
+    p5.add_run("VNTSZL74E71F205T\n").bold = True
+
+    p6 = d.add_paragraph()
+    p6.paragraph_format.line_spacing = 1
+    p6.alignment = 2
+    p6.style = d.styles['Normal']
+    p6.add_run("Spett.le                                       \n")
+    p6.add_run("la LUSac srl                              \n").bold = True
+    p6.add_run("via Defendente Sacchi 13          \n")
+    p6.add_run("27100 PAVIA                             \n\n")
+    p6.add_run("Partita IVA 02778180188          \n").bold = True
+    
+    p7 = d.add_paragraph()
+    p7.paragraph_format.line_spacing = 1
+    p7.alignment = 0
+    p7.style = d.styles['Normal']
+    p7.add_run("Ricevuta nr. "+str(numero)+" / "+date.today().strftime("%Y"))
+    p7.add_run("\n\n"+data)
+    p7.add_run("\n\nRicevo dalla Società la LUSac srl le somme sotto specificate a fronte delle prestazioni rientranti in rapporto di prestazione di lavoro autonomo occasionale*")
+
+    table2 = d.add_table(4,0, style="Light List")
+    table2.add_column(Mm(129.2))
+    table2.add_column(Mm(30))
+    table2.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+    row2 = table2.rows
+    row2[0].cells[0].text = "Tipologia della prestazione"
+    row2[0].cells[0].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
+    row2[0].cells[0].horizontal_alignment = WD_TABLE_ALIGNMENT.CENTER
+
+    row2[0].cells[1].text = "Compenso"
+    row2[0].cells[1].vertical_alignment = WD_TABLE_ALIGNMENT.CENTER
+    row2[0].cells[1].horizontal_alignment = WD_TABLE_ALIGNMENT.CENTER
+
+    table2.rows[0].height = Mm(10)
+    
+    
+
+    p8 = row2[1].cells[0].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 0
+    p8.style = d.styles['Normal']
+    p8.add_run("\n\n\n")
+    p8.add_run(causale)
+    p8.add_run("\n\n\n")
+
+
+    p8 = row2[1].cells[1].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 2
+    p8.style = d.styles['Normal']
+    p8.add_run("\n\n\n")
+    p8.add_run("€   %.2f" %(totale *1.25))
+    p8.add_run("\n\n\n")
+
+    p8 = row2[2].cells[0].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 2
+    p8.style = d.styles['Normal']
+    p8.add_run("Ritenuta d'acconto 20%")
+    p8.add_run("\n")
+
+    p8 = row2[2].cells[1].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 2
+    p8.style = d.styles['Normal']
+    p8.add_run("€   %.2f" %(totale *0.25))
+    p8.add_run("\n")
+    
+    p8 = row2[3].cells[0].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 2
+    p8.style = d.styles['Normal']
+    p8.add_run("Netto a pagare")
+    p8.add_run("\n")
+
+    p8 = row2[3].cells[1].add_paragraph()
+    p8.paragraph_format.line_spacing = 1
+    p8.alignment = 2
+    p8.style = d.styles['Normal']
+    p8.add_run("€   " + str(totale))
+    p8.add_run("\n")
+
+    p9 = d.add_paragraph()
+    p9.paragraph_format.line_spacing = 1
+    p9.alignment = 0
+    p9.style = d.styles['Normal']
+    p9.add_run("\nTale importo dovrà essere bonificato sul seguente conto corrente\n")
+    p9.add_run("Fineco Bank\n")
+    p9.add_run("Iban: IT95G0301503200000002578901\n")
+    p9.add_run("intestatario: Sara Ezelina Vantini\n")
+
+    #adding space between table and paragraphs
+    d.add_paragraph().add_run("\n\n\n\n\n")
+
+    p10 = d.add_paragraph()
+    p10.paragraph_format.line_spacing = 1
+    p10.alignment = 0
+    p10.style = d.styles['Normal']
+    p10.add_run("*Le eventuali marche da bollo per la contabilità sono a carico della stessa ditta committente.\n")
+    p10.add_run("La prestazione oggetto della presente nota è stata effettuata in via occasionale, contingente ed episodica; il relativo compenso è da inquadrare tra i redditi di cui all’art. 67 comma 1, lettera L, del D.P.R. 917/86 e, pertanto, esclusa dal campo di applicazione dell’I.V.A. ai sensi dell'art. 5 del D.P.R. n. 633 del 26 ottobre 1972 e successive modifiche ed integrazioni.")
+
+
+
+    return d
+
+# item_list = [[3, 5.0, 'Shopper upcycling'], [2, 4.0, 'Coppia lettere UNICA'], [10, 2.0, 'Sportina con tasca'],[6, 7.0, 'test1'],[9, 5.6, 'test2'],[1, 9.9, 'test3']]
+# causale = "Confezione di n. 5 borse LUSac"
+# numero = 5
+# create_receipt(item_list, numero, causale)
